da7171bc7be582cef3b35ee11fafe66896cb739d
notefox <kitsu-geissler@t-online.de>
2022-02-01 11:14:14 +0100

    Initial commit

---
+++ selfChat_569194/selfChat_569194App.swift
@@ -0,0 +1,18 @@
+//
+//  selfChat_569194App.swift
+//  selfChat_569194
+//
+//  Created by on 01.02.22.
+//
+//
+
+import SwiftUI
+
+@main
+struct selfChat_569194App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
